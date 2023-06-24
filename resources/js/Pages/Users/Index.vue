4dc483c1c5cf6a07038a34a69b1e1e188ba2c4e4
lar-asmaoui <asmaouilarbi35@gmail.com>
2023-06-24 12:28:59 +0100

    changes

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -24,6 +24,138 @@ export default {
         </button>
     </div>
 
+    <teleport to="body">
+        <Modal size="md" v-if="isModalOpen" @close="closeModal">
+            <template #header>
+                <div class="flex items-center text-lg">
+                    {{
+                        form.id
+                            ? "Mettre à jour Utilisateur"
+                            : "Ajouter Utilisateur"
+                    }}
+                </div>
+            </template>
+            <template #body>
+                <form
+                    class="space-y-2 px-2 lg:px-2 pb-2 sm:pb-2 xl:pb-2"
+                    @submit.prevent="submit"
+                >
+                    <div class="grid grid-cols-1 gap-4 lg:gap-6">
+                        <div>
+                            <label
+                                for="name"
+                                class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+                                >name
+                            </label>
+                            <input
+                                v-model="form.name"
+                                type="text"
+                                name="name"
+                                id="name"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            />
+                            <span
+                                v-if="form.errors.name"
+                                class="text-xs text-red-600 mt-1"
+                                id="hs-validation-name-error-helper"
+                            >
+                                {{ form.errors.name }}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-1 gap-4 lg:gap-6">
+                        <div>
+                            <label
+                                for="Email"
+                                class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+                                >Email
+                            </label>
+                            <input
+                                v-model="form.email"
+                                type="text"
+                                name="email"
+                                id="email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            />
+                            <span
+                                v-if="form.errors.email"
+                                class="text-xs text-red-600 mt-1"
+                                id="hs-validation-name-error-helper"
+                            >
+                                {{ form.errors.email }}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-1 gap-4 lg:gap-6">
+                        <div>
+                            <label
+                                for="mot de passe"
+                                class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+                                >Mot de passe
+                            </label>
+                            <input
+                                v-model="form.password"
+                                type="text"
+                                name="password"
+                                id="password"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            />
+                            <span
+                                v-if="form.errors.password"
+                                class="text-xs text-red-600 mt-1"
+                                id="hs-validation-name-error-helper"
+                            >
+                                {{ form.errors.password }}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-1 gap-4 lg:gap-6">
+                        <div>
+                            <label
+                                for="name"
+                                class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+                                >Role
+                            </label>
+                            <input
+                                v-model="form.role"
+                                type="text"
+                                name="role"
+                                id="role"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            />
+                            <span
+                                v-if="form.errors.role"
+                                class="text-xs text-red-600 mt-1"
+                                id="hs-validation-name-error-helper"
+                            >
+                                {{ form.errors.role }}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="mt-5 flex justify-end gap-x-2">
+                        <button
+                            @click="isModalOpen = false"
+                            type="button"
+                            class="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+                        >
+                            Annuler
+                        </button>
+                        <button
+                            type="submit"
+                            class="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+                        >
+                            {{ form.id ? "Mettre à jour" : "Enregistrer" }}
+                        </button>
+                    </div>
+                </form></template
+            ></Modal
+        >
+    </teleport>
+
     <div class="mt-4">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
             <the-card v-for="user in users" :key="user.id">
