3fb66130ffaa8940316cf707b8c6bec0bb34b302
Larbi Asmaoui <103370123+lar-asmaoui@users.noreply.github.com>
2023-10-15 23:11:27 +0100

    Update Association.php

--- app/Models/Association.php
+++ app/Models/Association.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Association extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 }
