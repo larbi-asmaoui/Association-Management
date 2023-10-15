749a3302d55963247815aaf129d7ba327a61feae
Larbi Asmaoui <103370123+lar-asmaoui@users.noreply.github.com>
2023-10-15 23:11:49 +0100

    Update Stock.php

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -6,10 +6,11 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Stock extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
 
 
